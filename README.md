d33ab0bf178d0037002936cf79dcb641f40586f8
Ayesh Karunaratne <ayesh@aye.sh>
2024-01-12 19:37:28 +0700

    Add initial stub

---
+++ README.md
@@ -0,0 +1,21 @@
+# PDO MySQL Subclass Polyfill
+
+[![Latest Stable Version](http://poser.pugx.org/polyfills/polyfills/pdo-mysql-subclass/v)](https://packagist.org/packages/polyfills/pdo-mysql-subclass) [![License](http://poser.pugx.org/polyfills/pdo-mysql-subclass/license)](https://packagist.org/packages/polyfills/pdo-mysql-subclass) [![PHP Version Require](http://poser.pugx.org/polyfills/pdo-mysql-subclass/require/php)](https://packagist.org/packages/polyfills/pdo-mysql-subclass)
+
+Provides user-land PHP polyfills for the MySQL subclass provided by PHP 8.4.
+
+Requires PHP 8,1, 8.2 or PHP 8.3 with `mbstring` extension. Not supported on PHP 8.4 because these functions are natively implemented in PHP 8.4.
+
+## Installation
+
+```bash
+composer require polyfills/pdo-mysql-subclass
+```
+
+## Usage
+
+
+## Contributions
+
+Contributions are welcome either as a GitHub issue or a PR to this repo.
+
---
+++ src/PdoMysql.php
@@ -0,0 +1,73 @@
+<?php
+class PdoMysql extends PDO {
+
+    public const int ATTR_USE_BUFFERED_QUERY = PDO::MYSQL_ATTR_USE_BUFFERED_QUERY;
+
+    /** @cvalue PDO_MYSQL_ATTR_LOCAL_INFILE */
+    public const int ATTR_LOCAL_INFILE = PDO::MYSQL_ATTR_LOCAL_INFILE;
+
+    /** @cvalue PDO_MYSQL_ATTR_INIT_COMMAND */
+    public const int ATTR_INIT_COMMAND = PDO::MYSQL_ATTR_INIT_COMMAND;
+
+#ifndef PDO_USE_MYSQLND
+    /** @cvalue PDO_MYSQL_ATTR_MAX_BUFFER_SIZE */
+    #public const int ATTR_MAX_BUFFER_SIZE = PDO:;
+
+    /** @cvalue PDO_MYSQL_ATTR_READ_DEFAULT_FILE */
+    #public const int ATTR_READ_DEFAULT_FILE = UNKNOWN;
+
+    /** @cvalue PDO_MYSQL_ATTR_READ_DEFAULT_GROUP */
+    #public const int ATTR_READ_DEFAULT_GROUP = UNKNOWN;
+#endif
+
+    /** @cvalue PDO_MYSQL_ATTR_COMPRESS */
+    public const int ATTR_COMPRESS = PDO::MYSQL_ATTR_COMPRESS;
+
+    /** @cvalue PDO_MYSQL_ATTR_DIRECT_QUERY */
+    public const int ATTR_DIRECT_QUERY = PDO::MYSQL_ATTR_DIRECT_QUERY;
+
+    /** @cvalue PDO_MYSQL_ATTR_FOUND_ROWS */
+    public const int ATTR_FOUND_ROWS = PDO::MYSQL_ATTR_FOUND_ROWS;
+
+    /** @cvalue PDO_MYSQL_ATTR_IGNORE_SPACE */
+    public const int ATTR_IGNORE_SPACE = PDO::MYSQL_ATTR_IGNORE_SPACE;
+
+    /** @cvalue PDO_MYSQL_ATTR_SSL_KEY */
+    public const int ATTR_SSL_KEY = PDO::MYSQL_ATTR_SSL_KEY;
+
+    /** @cvalue PDO_MYSQL_ATTR_SSL_CERT */
+    public const int ATTR_SSL_CERT = PDO::MYSQL_ATTR_SSL_CERT;
+
+    /** @cvalue PDO_MYSQL_ATTR_SSL_CA */
+    public const int ATTR_SSL_CA = PDO::MYSQL_ATTR_SSL_CA;
+
+    /** @cvalue PDO_MYSQL_ATTR_SSL_CAPATH */
+    public const int ATTR_SSL_CAPATH = PDO::MYSQL_ATTR_SSL_CAPATH;
+
+    /** @cvalue PDO_MYSQL_ATTR_SSL_CIPHER */
+    public const int ATTR_SSL_CIPHER = PDO::MYSQL_ATTR_SSL_CIPHER;
+
+#if MYSQL_VERSION_ID > 50605 || defined(PDO_USE_MYSQLND)
+    /** @cvalue PDO_MYSQL_ATTR_SERVER_PUBLIC_KEY */
+    public const int ATTR_SERVER_PUBLIC_KEY = PDO::MYSQL_ATTR_SERVER_PUBLIC_KEY;
+#endif
+
+    /** @cvalue PDO_MYSQL_ATTR_MULTI_STATEMENTS */
+    public const int ATTR_MULTI_STATEMENTS = PDO::MYSQL_ATTR_MULTI_STATEMENTS;
+
+    /** @cvalue PDO_MYSQL_ATTR_SSL_VERIFY_SERVER_CERT */
+    public const int ATTR_SSL_VERIFY_SERVER_CERT = PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT;
+
+#if MYSQL_VERSION_ID >= 80021 || defined(PDO_USE_MYSQLND)
+    /** @cvalue PDO_MYSQL_ATTR_LOCAL_INFILE_DIRECTORY */
+    public const int ATTR_LOCAL_INFILE_DIRECTORY = PDO::MYSQL_ATTR_LOCAL_INFILE_DIRECTORY;
+#endif
+    public static function connect(
+        string $dsn,
+        ?string $username = null,
+        #[\SensitiveParameter] ?string $password = null,
+        ?array $options = null
+    ): static {
+        return new static($dsn, $username, $password, $options, $options);
+    }
+}
